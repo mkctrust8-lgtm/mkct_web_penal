4dfbb82f4c43633e48d66a03ca6cac9765f2c1fd
Lalit Kumar <kumar932687@gmail.com>
2026-06-07 18:44:36 +0530

    updaeet code

---
+++ src/lib/constentData.js
@@ -0,0 +1,39 @@
+import certificateImg from "@/app/api/helperfile/Images/CertificateImg";
+import logo from "@/app/api/helperfile/Images/logo";
+import semkariLogo from "@/app/api/helperfile/Images/semkariLogo";
+
+export const TrsutData={
+    name:"मानव कल्याण चेरिटेबल ट्रस्ट (थराद)",
+    cityState:"गुजरात-राजस्थान",
+    address:"ऑफिस नं 70 दवे फार्म मु पो ता थराद जी बनासकांठा 385565",
+    contact:"9320387645 / 9725823111 / 9076970217",
+    contactPerson:"दिनेशभाई बी. मोदी",
+    trustPresident:"धीरजभाई एस. मोदी,सुरेशभाई एच. मोदी ",
+    frameImg:certificateImg,
+    email:"mkctrust8@gmail.com",
+    website:"",
+    regNo:"F /7141/B.K / 2025",
+    logo:logo,
+    RightLogo:null,
+    headerImg:"/Images/headerImg.png",
+    topTitle:[
+        "॥ ॐ जय श्री शामलाजी नमः ॥",
+        "॥ ॐ जय श्री गणेशाय नमः ॥",
+        "॥ ॐ जय श्री कुलदेवी माताय नमः ॥"
+    ]
+}
+
+export const pdfColors={
+bgColor:'#ffffff',
+borderColor:'#eb3694',
+headingColor:'#eb1e23',
+schemeColor:'#1a0f5e',
+infoLabelColor:'#EA1F25',
+infoValueColor:'#1a0f5e',
+filterLabelColor:'#8B0000',
+filterValueColor:'#1a0f5e',
+summaryColor:'#1a0f5e',
+totalCountColor:'#d4af37',
+programHeaderColor:'#1a0f5e',
+programHeaderBorderColor:'#d4af37',
+}
